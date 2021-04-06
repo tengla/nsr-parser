f9fde79bd0d21f03ebadb9f87e7bc15e9b67bc30
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2021-04-06 07:24:48 +0200

    feat(find): add regexp Name search

--- cmd/find/main.go
+++ cmd/find/main.go
@@ -7,13 +7,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 
 	"github.com/tengla/nsr-parser/stopplace"
 )
 
 var (
-	key = flag.String("key", "", "The key to look for")
-	val = flag.String("value", "", "The value to look for")
+	key     = flag.String("key", "", "The key to look for")
+	val     = flag.String("value", "", "The value to look for")
+	jsonout = flag.Bool("jsonout", false, "JSON output")
 )
 
 func failOnError(err error) {
@@ -23,6 +25,13 @@ func failOnError(err error) {
 }
 
 func printStop(sp stopplace.StopPlace) {
+	if *jsonout {
+		bytes, err := json.Marshal(sp)
+		failOnError(err)
+		fmt.Println(string(bytes))
+		fmt.Println("")
+		return
+	}
 	fmt.Println("")
 	fmt.Printf("ID='%s', Name='%s'\n ", sp.ID, sp.Name.Text)
 	for _, kv := range sp.KeyList.KeyValue {
@@ -38,7 +47,7 @@ func main() {
 	scanner := bufio.NewScanner(f)
 	onNewLine := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
 		for i := 0; i < len(data); i++ {
-			if data[i] == '\n' {
+			if data[i] == '\n' /* 10 newline*/ {
 				return i + 1, data[:i], nil
 			}
 		}
@@ -50,11 +59,24 @@ func main() {
 	scanner.Split(onNewLine)
 	for scanner.Scan() {
 		var sp stopplace.StopPlace
-		err := json.Unmarshal(scanner.Bytes(), &sp)
-		if err != nil {
+		bytes := scanner.Bytes()
+		if len(bytes) == 0 /*we hit the end*/ {
 			continue
 		}
-		if len(*key) > 0 && len(*val) < 1 {
+		err := json.Unmarshal(bytes, &sp)
+		if err != nil {
+			fmt.Printf("error parsing json %s\n", err.Error())
+			continue
+		}
+		if *key == "ID" {
+			printStop(sp)
+		} else if *key == "Name" && len(*val) > 0 {
+			re, err := regexp.Compile(*val)
+			failOnError(err)
+			if re.MatchString(sp.Name.Text) {
+				printStop(sp)
+			}
+		} else if len(*key) > 0 && len(*val) < 1 {
 			for _, kv := range sp.KeyList.KeyValue {
 				if kv.Key == *key {
 					printStop(sp)
