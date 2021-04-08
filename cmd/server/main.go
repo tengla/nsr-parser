e57dbca4186c12d57a17dddad6a2c40ad88dd4a6
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2021-04-08 22:50:24 +0200

    feat(server): add server and handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("server")
+	http.HandleFunc("/bar/id", func(rw http.ResponseWriter, r *http.Request) {
+		message := map[string]string{
+			"message": "Hello",
+		}
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(message)
+	})
+	log.Println("Starting server at :3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
