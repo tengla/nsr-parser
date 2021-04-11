c60caeda51bd3c8868618083f7f8dcedcbc6bbce
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2021-04-11 23:11:10 +0200

    test(ktx): add specs

--- cmd/ktx/main_test.go
+++ cmd/ktx/main_test.go
@@ -1,9 +1,24 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"testing"
+	"time"
+)
 
 func TestKtx(t *testing.T) {
 	t.Run("ktx", func(t *testing.T) {
-		main()
+		ctx, cancel := context.WithTimeout(
+			context.Background(),
+			time.Duration(time.Second*30))
+		defer cancel()
+		ch := mychan(ctx)
+		var b byte
+		for c := range ch {
+			b = c
+		}
+		if b != 'z' {
+			t.Fatal("expected a 'z'")
+		}
 	})
 }
