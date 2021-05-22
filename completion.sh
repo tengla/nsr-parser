09847ae6bf3f7233c512ae7560ed6709e2179cf0
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2021-05-22 13:32:06 +0200

    chore: remove completion file

--- completion.sh
+++
@@ -1,8 +0,0 @@
-function get-zsh-git-flow-completion () {
-  compl="$(brew list git-flow | grep completion.zsh)" 
-  if [ -n "$compl" ]; then
-    echo "Got completion $compl"
-    source $compl
-  fi
-}
-
